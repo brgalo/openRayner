eec47de423ca06c5bf27e5199c82c637c415264b
brgalo <martinhwehr@gmail.com>
2022-12-17 17:11:37 +0100

    finished 11

--- src/host/app.cpp
+++ src/host/app.cpp
@@ -1,6 +1,7 @@
 #include "app.hpp"
 
 #include "rendersystem.hpp"
+#include <memory>
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
@@ -41,20 +42,73 @@ void Application::run() {
   vkDeviceWaitIdle(device.device());
 }
 
+// temporary helper function, creates a 1x1x1 cube centered at offset
+std::unique_ptr<Geometry> createCubeModel(Device& device, glm::vec3 offset) {
+  std::vector<Geometry::Vertex> vertices{
+
+      // left face (white)
+      {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
+      {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
+      {{-.5f, -.5f, .5f}, {.9f, .9f, .9f}},
+      {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
+      {{-.5f, .5f, -.5f}, {.9f, .9f, .9f}},
+      {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
+
+      // right face (yellow)
+      {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
+      {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
+      {{.5f, -.5f, .5f}, {.8f, .8f, .1f}},
+      {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
+      {{.5f, .5f, -.5f}, {.8f, .8f, .1f}},
+      {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
+
+      // top face (orange, remember y axis points down)
+      {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+      {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+      {{-.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+      {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+      {{.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+      {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+
+      // bottom face (red)
+      {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+      {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
+      {{-.5f, .5f, .5f}, {.8f, .1f, .1f}},
+      {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+      {{.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+      {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
+
+      // nose face (blue)
+      {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+      {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+      {{-.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+      {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+      {{.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+      {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+
+      // tail face (green)
+      {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+      {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+      {{-.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+      {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+      {{.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+      {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+
+  };
+  for (auto& v : vertices) {
+    v.position += offset;
+  }
+  return std::make_unique<Geometry>(device, vertices);
+}
+
 void Application::loadOrayObjects() {
-  std::vector<Geometry::Vertex> vertices{{{0.0f, -.5f}, {1.0f, 0.0f, 0.0f}},
-                                         {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
-                                         {{-.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}};
-  auto geometry = std::make_shared<Geometry>(device, vertices);
+  std::shared_ptr<Geometry> geometry = createCubeModel(device, {.0f, .0f, .0f});
 
-  auto triangle = OrayObject::createOrayObject();
-  triangle.geom = geometry;
-  triangle.color = {.1f, 0.8f, 0.1f};
-
-  triangle.transform2d.translation.x = 0.2f;
-  triangle.transform2d.scale = {2.0f, 0.5f};
-  triangle.transform2d.rotation = 0.25f * glm::two_pi<float>();
-  orayObjects.push_back(std::move(triangle));
+  auto cube = OrayObject::createOrayObject();
+  cube.geom = geometry;
+  cube.transform.translation = {.0f, .0f, .5f};
+  cube.transform.scale = {.5f, .5f, .5f};
+  orayObjects.push_back(std::move(cube));
 }
 
 } // namespace oray
--- src/host/rendersystem.cpp
+++ src/host/rendersystem.cpp
@@ -20,8 +20,7 @@
 namespace oray {
 
 struct SimplePushConstantData {
-  glm::mat2 transform{1.f};
-  glm::vec2 offset;
+  glm::mat4 transform{1.f};
   alignas(16) glm::vec3 color;
 };
 
@@ -71,13 +70,18 @@ void RenderSystem::createPipeline(VkRenderPass renderPass) {
 void RenderSystem::renderOrayObjects(VkCommandBuffer commandBuffer,
                                      std::vector<OrayObject> &orayObjects) {
   graphicsPipeline->bind(commandBuffer);
+
   for (auto &obj : orayObjects) {
+    obj.transform.rotation.y =
+        glm::mod(obj.transform.rotation.y + 0.0001f, glm::two_pi<float>());
+    obj.transform.rotation.x =
+        glm::mod(obj.transform.rotation.x + 0.00005f, glm::two_pi<float>());
+    
     SimplePushConstantData push{};
-    push.offset = obj.transform2d.translation;
     push.color = obj.color;
-    push.transform = obj.transform2d.mat2();
+    push.transform = obj.transform.mat4();
     vkCmdPushConstants(commandBuffer, pipelineLayout,
-                       VK_SHADER_STAGE_VERTEX_BIT |
+                       VK_SHADER_STAGE_VERTEX_BIT | 
                            VK_SHADER_STAGE_FRAGMENT_BIT,
                        0, sizeof(SimplePushConstantData), &push);
     obj.geom->bind(commandBuffer);
